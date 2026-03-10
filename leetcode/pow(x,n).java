ab09cae87ded4bf63973a5d2c5f857aca918ff27
Kemba Devi <155751029+KembaDe@users.noreply.github.com>
2026-03-10 09:48:38 +0530

    Create pow(x,n).java

---
+++ leetcode/pow(x,n).java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // Use long to handle the overflow case when n = Integer.MIN_VALUE
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1;
+        double currentProduct = x;
+
+        while (N > 0) {
+            // If N is odd, multiply the result by the current product
+            if (N % 2 == 1) {
+                result *= currentProduct;
+            }
+            // Square the base and halve the exponent
+            currentProduct *= currentProduct;
+            N /= 2;
+        }
+
+        return result;
+    }
+}
