bcd92824b77b38f5cb226e6b8bac21e2fe18ea97
Kemba Devi <155751029+KembaDe@users.noreply.github.com>
2026-03-16 16:10:28 +0530

    Create Java Loopes II.java

---
+++ Hackerrank/Java Loopes II.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        
+        for(int i = 0; i < t; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int currentSum = a;
+            for(int j = 0; j < n; j++){
+                // Math.pow returns a double, so we cast to int
+                currentSum += (int)(Math.pow(2, j) * b);
+                System.out.print(currentSum + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
