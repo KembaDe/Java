82bf35425aa723bc102280711dd68d2e59ff40cf
Kemba Devi <155751029+KembaDe@users.noreply.github.com>
2026-03-31 10:28:12 +0530

    Update Java Regex 2.java

--- Hackerrank/Java Regex 2.java
+++ Hackerrank/Java Regex 2.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        // 1. The RegEx to match repeated words
+        String regex = "\\b(\\w+)(?:\\s+\\1\\b)+";
+        
+        // 2. Compile with CASE_INSENSITIVE flag
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                // 3. Replace the match with the first captured group
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
+}
 
